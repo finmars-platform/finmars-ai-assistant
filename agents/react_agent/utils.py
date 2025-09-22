bf0958bff02423302918c6618dc60dce7b97a8ba
Dmitrii Koriakov <dm.koryakov@gmail.com>
2025-09-23 01:18:03 +0200

    init_llm

--- agents/react_agent/utils.py
+++ agents/react_agent/utils.py
@@ -1,6 +1,9 @@
+import traceback
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 
+from libs.logger.logger import logger
 from libs.utils.key_manager import get_api_key
 
 
@@ -8,7 +11,9 @@ def init_llm(task_solver_config: dict, kwargs: dict = dict()):
     # WARNING: Keep `kwargs` as it is, overwise you will get an errpr
     # NameError: Fields must not use names with leading underscores; e.g., use 'pydantic_extra__' instead of '__pydantic_extra__'.
     clean_kwargs = {k: v for k, v in kwargs.items() if not k.startswith("_")}
-    task_solver_config = {k: v for k, v in task_solver_config.items() if not k.startswith("_")}
+    task_solver_config = {
+        k: v for k, v in task_solver_config.items() if not k.startswith("_")
+    }
     is_google_provider = task_solver_config.get("is_google_provider", False)
     if is_google_provider:
         # Use ChatGoogleGenerativeAI for Google models
@@ -18,7 +23,9 @@ def init_llm(task_solver_config: dict, kwargs: dict = dict()):
             "thinking_budget": task_solver_config.get("thinking_budget", -1),
             "include_thoughts": task_solver_config.get("include_thoughts", True),
         }
-        executor_llm = ChatGoogleGenerativeAI(**{**task_solver_llm_config, **clean_kwargs})
+        executor_llm = ChatGoogleGenerativeAI(
+            **{**task_solver_llm_config, **clean_kwargs}
+        )
     else:
         # Use ChatOpenAI for OpenAI models
         task_solver_llm_config = {
@@ -27,5 +34,12 @@ def init_llm(task_solver_config: dict, kwargs: dict = dict()):
             "temperature": task_solver_config.get("temperature"),
             "base_url": task_solver_config.get("base_url"),
         }
-        executor_llm = ChatOpenAI(**{**task_solver_llm_config, **clean_kwargs})
+        try:
+            executor_llm = ChatOpenAI(**{**task_solver_llm_config, **clean_kwargs})
+        except NameError as e:
+            exc = traceback.format_exc()
+            logger.error(exc)
+            logger.warning("TRY AGAIN")
+            executor_llm = ChatOpenAI(**{**task_solver_llm_config, **clean_kwargs})
+
     return executor_llm
